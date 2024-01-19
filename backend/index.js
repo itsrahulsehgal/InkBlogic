a4296f0943a83add439971702b1e4a71ee35acb9
Rahul Sehgal <104090821+itsrahulsehgal@users.noreply.github.com>
2024-01-19 19:48:14 +0530

    Vercel.json added in backend'

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const connectDB = async () => {
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ credentials: true, origin: 'https://ink-blogic.vercel.app', methods: ["POST", "GET"] }));
 
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
